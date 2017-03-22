ea767a2d719f156d08999ab3999709320a5bd76d
Evan Berkowitz <git@evanberkowitz.com>
2017-03-22 11:31:52 +0100

    First spin basis description.

---
+++ include/Spin.h
@@ -0,0 +1,40 @@
+#ifndef _HADRON_PDFS_SPIN
+#define _HADRON_PDFS_SPIN
+
+enum class SPIN_BASIS{
+    DIRAC_PAULI     = 0,
+    DEGRAND_ROSSI   = 1,
+    CHROMA          = 1,
+    QDP             = 1
+};
+
+
+std::string inline to_string(const SPIN_BASIS &basis){
+    switch(basis){
+        case SPIN_BASIS::DIRAC_PAULI:       return "DIRAC_PAULI";               break;
+        case SPIN_BASIS::DEGRAND_ROSSI:     return "DEGRAND_ROSSI";             break;
+        
+        default:                            return "unknown spin basis";
+    };
+}
+
+
+void spin_basis_transform(LatticePropagator& prop, const SPIN_BASIS new_basis, const SPIN_BASIS old_basis){
+    SpinMatrix rotator;
+         if ( new_basis     == old_basis){ return; }
+    // Chroma has a built-in for converting from Dirac-Pauli to Degrand-Rossi
+    else if ( SPIN_BASIS::DIRAC_PAULI   == new_basis    &&  SPIN_BASIS::DEGRAND_ROSSI   == old_basis    ){ rotator = adj(PauliToDRMat());   }
+    else if ( SPIN_BASIS::DEGRAND_ROSSI == new_basis    &&  SPIN_BASIS::DIRAC_PAULI     == old_basis    ){ rotator = PauliToDRMat();        }
+    
+    else    {   
+        QDPIO::cout << "Spin basis transformation is unknown." << std::endl;
+        QDP_abort(EXIT_FAILURE);
+    }
+
+    LatticePropagator temp;
+    temp = rotator * prop * adj(rotator);
+    prop = temp;
+    return;
+}
+
+#endif /* end of include guard: _HADRON_PDFS_SPIN */
