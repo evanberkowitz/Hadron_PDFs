2a4829feeef9cc84a80fdc43df941bf56452d558
Evan Berkowitz <git@evanberkowitz.com>
2017-04-21 15:11:52 +0200

    Reorganize SpinBasis

--- include/SpinBasis.h
+++ include/SpinBasis.h
@@ -9,32 +9,8 @@ enum class SPIN_BASIS{
 };
 
 
-std::string inline to_string(const SPIN_BASIS &basis){
-    switch(basis){
-        case SPIN_BASIS::DIRAC_PAULI:       return "DIRAC_PAULI";               break;
-        case SPIN_BASIS::DEGRAND_ROSSI:     return "DEGRAND_ROSSI";             break;
-        
-        default:                            return "unknown spin basis";
-    };
-}
+std::string inline to_string(const SPIN_BASIS &basis);
 
-
-void spin_basis_transform(LatticePropagator& prop, const SPIN_BASIS new_basis, const SPIN_BASIS old_basis){
-    SpinMatrix rotator;
-         if ( new_basis     == old_basis){ return; }
-    // Chroma has a built-in for converting from Dirac-Pauli to Degrand-Rossi
-    else if ( SPIN_BASIS::DIRAC_PAULI   == new_basis    &&  SPIN_BASIS::DEGRAND_ROSSI   == old_basis    ){ rotator = adj(PauliToDRMat());   }
-    else if ( SPIN_BASIS::DEGRAND_ROSSI == new_basis    &&  SPIN_BASIS::DIRAC_PAULI     == old_basis    ){ rotator = PauliToDRMat();        }
-    
-    else    {   
-        QDPIO::cout << "Spin basis transformation is unknown." << std::endl;
-        QDP_abort(EXIT_FAILURE);
-    }
-
-    LatticePropagator temp;
-    temp = rotator * prop * adj(rotator);
-    prop = temp;
-    return;
-}
+void spin_basis_transform(LatticePropagator& prop, const SPIN_BASIS new_basis, const SPIN_BASIS old_basis);
 
 #endif /* end of include guard: _HADRON_PDFS_SPIN_BASIS */
