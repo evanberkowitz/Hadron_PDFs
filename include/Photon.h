7131f3e6659cb50e5377815a2aa0880a5fddd851
Evan Berkowitz <git@evanberkowitz.com>
2018-01-24 13:35:35 +0100

    Align blank lines

--- include/Photon.h
+++ include/Photon.h
@@ -5,17 +5,17 @@ LatticeComplex phases(const multi1d<int> &momentum){
     LatticeReal argument = zero;
     Real two_pi = Chroma::twopi;
     Real norm = 1.0;
-
+    
     for_direction(d){
         // Calculate p.x:
         argument += LatticeReal((two_pi * Layout::latticeCoordinate(d) * momentum[d]) / Layout::lattSize()[d]);
         norm     *= Real(Layout::lattSize()[d]);
     }
-
+    
     LatticeComplex b = zero;
-
+    
     b = cmplx( cos(argument), sin(argument) ) ; // / norm;
-
+    
     return b;
 }
 
